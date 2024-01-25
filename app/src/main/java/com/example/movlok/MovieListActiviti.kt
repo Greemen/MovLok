32dd17397b4b2e838e2f3e7dd033449f951fc1ca
Jan <j_andrysek@utb.cz>
2024-01-25 10:32:50 +0100

    New response finding by ID

--- app/src/main/java/com/example/movlok/MovieListActiviti.kt
+++ app/src/main/java/com/example/movlok/MovieListActiviti.kt
@@ -28,48 +28,85 @@ class MovieListActiviti : AppCompatActivity() {
         // Set up a click listener for the button
         button.setOnClickListener {
             Log.v("Tag", "Click")
-            getRetrofitResponse()
+            //GetRetrofitResponse()
+            GetRetrofitResponseAccordingToID()
         }
     }
-}
 
-private fun getRetrofitResponse() {
-    // Creating an instance of the Servicey class to get the MovieApi
-    val servicey = Servicey()
-    val movieApi = servicey.getMovieApi()
+    private fun GetRetrofitResponse() {
+        // Creating an instance of the Servicey class to get the MovieApi
+        val servicey = Servicey()
+        val movieApi = servicey.getMovieApi()
 
-    // Enqueueing the Retrofit call
-    val responseCall = movieApi.searchMovie(
-        Credentials.API_KEY,
-        "Jack Reacher",
-        "1")
+        // Enqueueing the Retrofit call
+        val responseCall = movieApi.searchMovie(
+            Credentials.API_KEY,
+            "Action",
+            "1")
 
-    responseCall.enqueue(object : Callback<MovieSearchResponse> {
-        override fun onResponse(
-            call: Call<MovieSearchResponse>,
-            response: Response<MovieSearchResponse>
-        ) {
-            if (response.code() == 200) {
-                val movies: List<MovieModel> = response.body()?.movies ?: listOf()
+        responseCall.enqueue(object : Callback<MovieSearchResponse> {
+            override fun onResponse(
+                call: Call<MovieSearchResponse>,
+                response: Response<MovieSearchResponse>
+            ) {
+                if (response.isSuccessful) {
+                    val movies: List<MovieModel> = response.body()?.movies ?: listOf()
 
-                // Log the whole response
-                Log.v("Tag", "The response: ${response.body().toString()}")
+                    // Log the whole response
+                    //Log.v("Tag", "The response: ${response.body().toString()}")
 
-                // Iterate through the movies and log their release date
-                for (movie in movies) {
-                    Log.v("Tag", "The release date: ${movie.release_date}")
+                    // Iterate through the movies and log their release date
+                    for (movie in movies) {
+                        Log.v("Tag", "Name: ${movie.title}" + " ${movie.release_date}")
                     }
 
-                // Handle the response here, such as updating UI or storing the data
-            } else {
-                // Handle the error scenario, such as displaying an error message
-                Log.e("Tag", "Error: ${response.errorBody()?.string()}")
+                    // Handle the response here, such as updating UI or storing the data
+                } else {
+                    // Handle the error scenario, such as displaying an error message
+                    Log.e("Tag", "Error: ${response.errorBody()?.string()}")
+                }
             }
-        }
 
-        override fun onFailure(call: Call<MovieSearchResponse>, t: Throwable) {
-            // Handle failure, such as network error, logging the exception
-            Log.e("Tag", "Failure: ${t.message}")
-        }
-    })
+            override fun onFailure(call: Call<MovieSearchResponse>, t: Throwable) {
+                // Handle failure, such as network error, logging the exception
+                Log.e("Tag", "Failure: ${t.message}")
+            }
+        })
+    }
+
+    private fun GetRetrofitResponseAccordingToID(){
+        val servicey = Servicey()
+        val movieApi = servicey.getMovieApi()
+
+        // Enqueueing the Retrofit call
+        val responseCall = movieApi.getMovie(
+            343611,
+            Credentials.API_KEY
+        )
+
+        responseCall.enqueue(object : Callback<MovieModel> {
+            override fun onResponse(
+                call: Call<MovieModel>,
+                response: Response<MovieModel>
+            ) {
+                if (response.isSuccessful) {
+                    // Log the movie title
+                    val movieTitle = response.body()?.title
+                    Log.v("Tag", "Movie Title: $movieTitle")
+                } else {
+                    // Log the error body
+                    val errorBody = response.errorBody()?.string()
+                    Log.e("Tag", "Error: $errorBody")
+                }
+            }
+
+            override fun onFailure(call: Call<MovieModel>, t: Throwable) {
+                // Handle failure, such as network error, logging the exception
+                Log.e("Tag", "Failure: ${t.message}")
+            }
+        })
+    }
+
 }
+
+
--- app/src/main/java/com/example/movlok/utils/MovieApi.kt
+++ app/src/main/java/com/example/movlok/utils/MovieApi.kt
@@ -1,8 +1,10 @@
 package com.example.movlok.utils
 
+import com.example.movlok.models.MovieModel
 import com.example.movlok.response.MovieSearchResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface MovieApi {
@@ -15,4 +17,10 @@ interface MovieApi {
         @Query("page") page: String
     ): Call<MovieSearchResponse>
 
+    // Search using ID  550 = Fight Club
+    @GET("3/movie/{movie_id}?")
+    fun getMovie(
+        @Path("movie_id") movie_id: Int,
+        @Query("api_key") api_key: String
+    ): Call<MovieModel>
 }
